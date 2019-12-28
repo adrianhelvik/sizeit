58ce6293ec6c1bdc3a4ac2f104d2a29f5c01eec7
Adrian Helvik <adrian.helvik@specifique.no>
2019-12-28 13:27:53 +0100

    Optimization

--- index.js
+++ index.js
@@ -5,16 +5,26 @@ const fs = require('fs')
 
 const rootDir = process.argv[2] || process.cwd()
 
-const scan = async (dir = rootDir, contents = {}) => {
-  const stats = await fs.promises.lstat(dir)
+const isProfiling = process.env.PROFILE_SIZEIT === 'true'
 
-  if (!stats.isDirectory()) {
+const scan = async (dir = rootDir, contents = {}) => {
+  let nonDirectoryStats = false
+
+  const files = await fs.promises.readdir(dir).catch(async e => {
+    const stats = await fs.promises.lstat(dir)
+
+    if (!stats.isDirectory()) {
+      nonDirectoryStats = stats
+    } else {
+      throw e
+    }
+  })
+
+  if (nonDirectoryStats) {
     const tmp = dir.split('/')
     const folderName = tmp[tmp.length - 1]
-    return { [folderName]: stats.size }
+    return { [folderName]: nonDirectoryStats.size }
   }
-
-  const files = await fs.promises.readdir(dir)
 
   await Promise.all(
     files.map(async file => {
@@ -24,9 +34,7 @@ const scan = async (dir = rootDir, contents = {}) => {
       if (stats.isDirectory()) {
         try {
           contents[file] = await scan(absFile)
-        } catch (e) {
-          return
-        }
+        } catch (e) {}
       } else {
         contents[file] = stats.size
       }
@@ -40,7 +48,16 @@ process.on('unhandledRejection', e => {
   throw e
 })
 
+if (isProfiling) {
+  var start = new Date()
+}
+
 scan().then(contents => {
+  if (isProfiling) {
+    const end = new Date()
+    console.log(`Scan duration: ${fmtTime(end - start)}\n`)
+  }
+
   const total = {}
   for (const key of Object.keys(contents)) {
     total[key] = (function sum(contents) {
@@ -61,6 +78,11 @@ scan().then(contents => {
       `${String(fmtSize(total[folder])).padStart(size, ' ')}   ${folder}`,
     )
   }
+
+  if (isProfiling) {
+    const end = new Date()
+    console.log(`\nFull duration: ${fmtTime(end - start)}`)
+  }
 })
 
 function fmtSize(size) {
@@ -68,4 +90,11 @@ function fmtSize(size) {
   if (size > 1024 ** 2) return (size / 1024 ** 2).toFixed(1) + 'MiB'
   if (size > 1024) return (size / 1024).toFixed(1) + 'KiB'
   return size + 'B  '
+}
+
+function fmtTime(timeInMillis) {
+  if (timeInMillis > 300) {
+    return (timeInMillis / 1000).toFixed(3) + 's'
+  }
+  return timeInMillis + 'ms'
 }
